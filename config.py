5be80e073d5c904c77fb89d3e58760a41eba096a
EitanKats <eitan.kats96@gmail.com>
2018-11-09 16:18:53 +0200

    added a feature that takes support taking the host of the docker from the environmental variable.
    the data that is being scraped is now in the data folder

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
-FORBIDDEN_USERS = ['anonymous', 'guest', 'unknown']
+import os
 
-PROXIES = {'http': 'socks5h://localhost:9150', 'https': 'socks5h://localhost:9150'}
+PROXY_PORT = 9150
+
+PROXIES = {'http': 'socks5h://localhost:{0}'.format(PROXY_PORT),
+           'https': 'socks5h://localhost:{0}'.format(PROXY_PORT)}
+
+DOCKER_HOST = os.getenv('DOCKER_HOST')
+if DOCKER_HOST:
+    PROXIES = {'http': 'socks5h://{0}:{1}'.format(DOCKER_HOST, PROXY_PORT),
+               'https': 'socks5h://{0}:{1}'.format(DOCKER_HOST, PROXY_PORT)}
+
+FORBIDDEN_USERS = ['anonymous', 'guest', 'unknown']
 
 CRAWL_URL = 'http://nzxj65x32vh2fkhk.onion/all'
 
 ARTICLE_DATE_FORMAT = 'DD MMM YYYY, HH:mm:ss'
 
-DATABASE_PATH = 'articles.json'
+DATABASE_PATH = 'data/articles.json'
 
 TABLE_NAME = 'articles'
